b11bce9a5c8766b06483a4f0709f498095cf20e5
hejhansson <hejhansson@me.com>
2015-11-18 16:48:55 +0100

    Nytt projekt: folktandvården

--- js/main.js
+++ js/main.js
@@ -6,40 +6,29 @@
 	var windowWidth = getWidth();
 
 	if(windowWidth > 98110) {
-		$( '.portfolio_item' ).hover(function() {
-			info.find('h2').html($(this).data('client'));
-			info.find('p').html($(this).data('info'));
-			info.find('span').html('<small>Tryck på bilden för att besöka sidan.</small>');
+		console.log("hej");
 
-		});
+		$('.portfolio_item').find('.image').click(function() {
+			$(this).toggleClass('active');
+			$('.portfolio').toggleClass('active');
+				$('.portfolio_item').each(function() {
+						$(this).toggleClass('hidden');
+				});
 
-		$( '.portfolio_item' ).mouseout(function() {
-			info.find('h2').html('');
-			info.find('p').html('');
-			//info.find('p').html('Håll musen över en bild för att se mer information om projektet.');
-			info.find('span').html('');
-		});
-	}
+				$('.heading').toggle();
 
-	if(windowWidth > 11980) {
-		var lastScrollTop = 0;
-		$(window).scroll(function(event){
-		   var st = $(this).scrollTop();
-		   if (st > lastScrollTop){
-		       console.log('down');
-					 $('.site-header .col-sm-3').css('width', '5%');
-					 $('.site-header .col-sm-3 a').html('AH');
-					 $('.site-header').addClass('lighter');
+				$(this).parent().toggleClass('hidden');
+				$(this).parent().toggleClass('col-md-12');
 
-		   } else {
+			if($(this).parent().find('.portfolio_item__info').is(":visible")) {
+				$(this).parent().find('.portfolio_item__info').hide();
 
-					setTimeout(function(){
-						$('.site-header').removeClass('lighter');
-			      $('.site-header .col-sm-3 a').html('Alexander Hansson');
-					}, 200);
-					$('.site-header .col-sm-3').css('width', '25%');
-		   }
-		   lastScrollTop = st;
+			}
+			else {
+
+				$(this).parent().find('.portfolio_item__info').show();
+			}
+
 		});
 	}
 
